a474c2b2c7b76ee6eb1f5f932e86833c4c07e056
KacperM-02 <240751@edu.p.lodz.pl>
2025-04-23 14:45:24 +0200

    Added task data class

---
+++ app/src/main/java/com/example/todo/data/Task.kt
@@ -0,0 +1,6 @@
+package com.example.todo.data
+
+data class Task(val id: Int, val status: Int, val taskDescription: String, val creationTime: String, val executionDate: String, val taskNotification: Int, val taskCategory: String)
+{
+
+}

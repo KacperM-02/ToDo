96ca9584bcc0f2fdd5c59040f4e8dac28edb4425
KacperM-02 <240751@edu.p.lodz.pl>
2025-05-23 15:57:00 +0200

    Initializing fragment details

--- app/src/main/java/com/example/todo/fragments/details/FragmentDetails.kt
+++ app/src/main/java/com/example/todo/fragments/details/FragmentDetails.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.example.todo.R
 import com.example.todo.databinding.FragmentDetailsBinding
 
 
@@ -13,7 +14,7 @@ class FragmentDetails : Fragment() {
 
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
-    private val args : FragmentDetailsArgs by navArgs()
+    private val args: FragmentDetailsArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,7 +23,22 @@ class FragmentDetails : Fragment() {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         val taskDetails = args.taskDetails
 
-        binding.taskTitleInput.text = taskDetails.taskTitle
+        binding.taskTitleInput.setText(taskDetails.taskTitle)
+        binding.taskStatusText.text = getString(
+            R.string.task_status,
+            if (taskDetails.taskStatus == 1) "Done" else "Undone"
+        )
+        binding.taskCreationTimeText.text = getString(
+            R.string.task_status,
+            taskDetails.taskCreationTime
+        )
+        binding.taskNotificationToggle.isChecked =
+            taskDetails.taskNotification == 1
+        binding.taskDescriptionInput.setText(taskDetails.taskDescription)
+        binding.taskExecutionDate.setText(taskDetails.taskExecutionDate)
+        binding.taskCategoryDropdown.setText(taskDetails.taskCategory)
+
+
         return binding.root
     }
 
